efb04333c556db5b73217642453a6229e726adb3
surajgirioffl <78554012+surajgirioffl@users.noreply.github.com>
2021-05-15 02:45:36 +0530

    Add files via upload

---
+++ 01) To find absolute value of given number.c
@@ -0,0 +1,24 @@
+//c language
+/*#1) All In One C Programs (mixture of any of concepts of C)>*/
+/*1) To find absolute value of given number*/
+//85
+/*absolute value (Distance of written value from zero. It's always positive because distace never negative. */
+//Problem on absolute value
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("\nwrite any number to display its absolute value: ");
+	scanf("%d", &n);
+	if (n > 0)
+		printf("\nabsolute value is %d", n);
+	else
+		printf("\nabolute value is %d", -n);
+
+	printf("\nThanking You");
+	return 0;
+}
+/*
+author- suraj kumar giri
+date- 9th may 2021
+*/
