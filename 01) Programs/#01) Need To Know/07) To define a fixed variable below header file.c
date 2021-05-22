34eee465a00a17c9614040d0acea44319356729f
surajgirioffl <78554012+surajgirioffl@users.noreply.github.com>
2021-05-23 01:59:31 +0530

    Add files via upload

---
+++ 01) Programs/#01) Need To Know/07) To define a fixed variable below header file.c
@@ -0,0 +1,18 @@
+//c language
+/*1) Program> #1) Need to know> */
+/*07) To define a fixed variable below header file */
+// 97
+#include <stdio.h>
+#define SURAJ "This is suraj kumar giri"
+/*In case of symbolic constant(#define name character/integer/string) 
+termination is not allowed. so we can not terminate during defining of fixed 
+constant that will be use inside the program*/
+void main()
+{
+	printf("%s", SURAJ);
+}
+/*
+authod- suraj kumar giri
+date- 22nd may 2021
+purpose- simply to learn the concept of fixed constant that can we use any where in the program
+*/
