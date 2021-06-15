a3192f9b207d4cf6dd082dc44eee11c19c32f60b
surajgirioffl <78554012+surajgirioffl@users.noreply.github.com>
2021-06-15 12:15:19 +0530

    Add files via upload

---
+++ 02) Lab or Assignment works/03) Assignment 3.c
@@ -0,0 +1,458 @@
+//c language
+/*02) Lab or Assignment work> */
+/*03) Assignment 3.c*/
+//100
+//asignment 3
+//Done
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+int number();
+void even_odd(int a);
+void to_check(int a);
+void sum_digit(int a);
+void inverse(int a);
+void palindrome(int a);
+void armstrong(int a);
+void factorial(int a);
+void fibonacci(int a);
+void table(int a);
+void prime(int a);
+void div_6(int a);
+void perfect(int a);
+void root(int a);
+void factor(int a);
+void sq_cu(int a);
+void series(int a);
+void shape1(int b);
+void shape2(int b);
+void shape3(int b);
+void shape4(int b);
+int main()
+{
+	int n, num;
+	char ch[4];
+	do
+	{
+	again:
+		puts("01. To check the entered number is odd or even");
+		puts("02. To check the entered number is positive, negative or neither negative nor positive");
+		puts("03. To find sum of digits of entered number");
+		puts("04. To find inverse of enterd number");
+		puts("05. To check the entered number is palindrome or not");
+		puts("06. To check the entered number is Armstrong or not");
+		puts("07. To find factorial of entered number");
+		puts("08. To find Fibonacci series up to enterd number");
+		puts("09. To find the table of given number and find it's sum");
+		puts("10. To check given number is prime or not");
+		puts("11. To check given number is divisible by 6 or not");
+		puts("12. To check given number is perfect or not");
+		puts("13. To find the squre root of given number");
+		puts("14. To find factors of given number");
+		puts("15. To find squre and cube of given number");
+		puts("16. To Generate Series up to entered number times\n");
+		puts("Now, write your choice:");
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:
+			num = number();
+			even_odd(num);
+			break;
+
+		case 2:
+			num = number();
+			to_check(num);
+			break;
+
+		case 3:
+			num = number();
+			sum_digit(num);
+			break;
+
+		case 4:
+			num = number();
+			inverse(num);
+			break;
+
+		case 5:
+			num = number();
+			palindrome(num);
+			break;
+
+		case 6:
+			num = number();
+			armstrong(num);
+			break;
+
+		case 7:
+			num = number();
+			factorial(num);
+			break;
+
+		case 8:
+			num = number();
+			fibonacci(num);
+			break;
+
+		case 9:
+			num = number();
+			table(num);
+			break;
+
+		case 10:
+			num = number();
+			prime(num);
+			break;
+
+		case 11:
+			num = number();
+			div_6(num);
+			break;
+
+		case 12:
+			num = number();
+			perfect(num);
+			break;
+
+		case 13:
+			num = number();
+			root(num);
+			break;
+
+		case 14:
+			num = number();
+			factor(num);
+			break;
+
+		case 15:
+			num = number();
+			sq_cu(num);
+			break;
+
+		case 16:
+			num = number();
+			series(num);
+			break;
+
+		default:
+			clrscr();
+			printf("\nYou have selected wrong choice. Please select correct one:\n");
+			goto again;
+		}
+		puts("\n\nDo you want to restart the program: yes or no");
+		printf("write your choice:");
+		scanf("%s", ch);
+	} while (ch[0] == 'y' || ch[0] == 'Y');
+	printf("\nThanking You. Bye Bye....");
+
+	return 0;
+}
+
+//0
+int number()
+{
+	int a;
+	puts("write the number:");
+	scanf("%d", &a);
+	return a;
+}
+
+//1
+void even_odd(int a)
+{
+	if (a == 0)
+		printf("\n0 even when divided by 2 and odd when divide by other than 2");
+	else if (a % 2 == 0)
+		printf("\n%d is an even number", a);
+	else if (a % 2 != 0)
+		printf("\n%d is an odd number", a);
+}
+//2
+void to_check(int a)
+{
+	if (a > 0)
+		printf("\n%d is positive number", a);
+	else if (a == 0)
+		printf("\n%d is neither positive nor negative", a);
+	else
+		printf("\n%d is negative number", a);
+}
+//3
+void sum_digit(int a)
+{
+	int sum = 0;
+	int r, z;
+	z = a;
+	for (int i = 0; a > 0; i++)
+	{
+		r = a % 10;
+		sum = sum + r;
+		a = a / 10;
+	}
+	printf("\nsum of digits of number %d is %d", z, sum);
+}
+//4
+void inverse(int a)
+{
+	printf("\n");
+	int r;
+	int z;
+	z = a;
+	for (int i = 0; a > 0; i++)
+	{
+		r = a % 10;
+		printf("%d", r);
+		a = a / 10;
+	}
+	printf(" is the reverse of %d", z);
+}
+//5
+void palindrome(int a)
+{
+	int r, z, check = 0;
+	int sum = 0;
+	z = a;
+	for (int i = 0; a > 0; i++)
+	{
+		r = a % 10;
+		sum = sum + r;
+		check = (check * 10) + r;
+		a = a / 10;
+	}
+	if (check == z)
+		printf("\n%d is a Palindrome Number with sum of digits equal to %d", z, sum);
+	else
+		printf("\n%d is not a Palindrome Number", z);
+}
+//6
+void armstrong(int a)
+{
+	int arm = 0, num_digit;
+	int r, i;
+	int z;
+	z = a;
+	for (i = 0; a > 0; i++)
+	{
+		r = a % 10;
+		a = a / 10;
+	}
+	//a get lost till here. so we refill the a.
+	a = z;
+	num_digit = i; //i ka value increse hone ke bad condition check karega. so we don't use i+1;
+	for (i = 0; a > 0; i++)
+	{
+		r = a % 10;
+		arm = arm + pow(r, num_digit);
+		a = a / 10;
+	}
+	if (arm == z)
+		printf("\n%d is an Armstrong Number", z);
+	else
+		printf("\n%d is not an Armstrong Number", z);
+}
+//7
+void factorial(int a)
+{
+	int fact = 1;
+	int z;
+	z = a;
+	for (int i = 0; a > 1; i++)
+	{
+		fact = fact * a;
+		a--;
+	}
+	printf("\nFactorial of %d is %d", z, fact);
+}
+//8
+void fibonacci(int a)
+{
+	printf("\n");
+	int p[a];
+	p[0] = 0;
+	p[1] = 1;
+	printf("\n%d %d", p[0], p[1]);
+	for (int i = 2; i < a; i++)
+	{
+		p[i] = p[i - 1] + p[i - 2];
+		printf(" %d", p[i]);
+	}
+}
+//9
+void table(int a)
+{
+	printf("\nTable of %d is following", a);
+	for (int i = 1; i < 11; i++)
+		printf("\n%dx%d=%d", a, i, (a * i));
+	printf("\nsum of table of %d is %d", a, (a * 55));
+}
+//10
+void prime(int a)
+{
+	int i, k = 0;
+	if (a == 2)
+		printf("\n2 is a prime number");
+	else if (a > 2)
+	{
+		for (i = 2; i <= (int)a / 2; i++)
+		{
+			if (a % i == 0)
+
+				k = k + 1;
+		}
+		if (k == 0)
+			printf("\n%d is a prime number", a);
+		else
+			printf("\n%d is not a prime number", a);
+	}
+	else
+		printf("%d is not prime number", a);
+}
+//11
+void div_6(int a)
+{
+	if (a % 6 == 0)
+		printf("\n%d is divided by 6", a);
+	else
+		printf("\n%d is not divided by 6", a);
+}
+//12
+void perfect(int a)
+{
+	int sum = 0;
+	for (int i = 1; i < a; i++)
+	{
+		if (a % i == 0)
+			sum = sum + i;
+	}
+	if (sum == a)
+		printf("\n%d is perfect number", a);
+	else
+		printf("\n%d is not a perfect number", a);
+}
+//13
+void root(int a)
+{
+	printf("\n%f is the squre root of %d", sqrt(a), a);
+}
+
+//14
+void factor(int a)
+{
+	printf("\nFollwings are the factors of %d\n", a);
+	for (int i = 1; i < a + 1; i++)
+	{
+		if (a % i == 0)
+			printf(" %d ", i);
+	}
+}
+
+//15
+void sq_cu(int a)
+{
+	printf("\n%f is the squre of %d\n%f is the cube of %d", pow((float)a, 2), a, pow((float)a, 3), a);
+}
+
+//16
+void series(int a)
+{
+	int n;
+again1:
+	puts("01. Left sided ladder with * shapes");
+	puts("02. Right sided laddr with * shapes");
+	puts("03. Left Sided Ladder with ABC...");
+	puts("04. Squre Using * shapes");
+	puts("\nPlease write your choice:");
+	scanf("%d", &n);
+	switch (n)
+	{
+	case 1:
+		shape1(a);
+		break;
+
+	case 2:
+		shape2(a);
+		break;
+
+	case 3:
+		shape3(a);
+		break;
+
+	case 4:
+		shape4(a);
+		break;
+
+	default:
+		clrscr();
+		printf("\nyou have selected wrong choice. Please reselect correct one");
+		goto again1;
+		break;
+	}
+}
+void shape1(int b)
+{
+	puts("\nRequire Pattern Is Following.\n");
+	for (int i = 0; i < b; i++)
+	{
+		for (int j = 0; j <= i; j++)
+			printf(" * ");
+		printf("\n");
+	}
+}
+
+void shape2(int b)
+{
+	puts("\nRequire Pattern Is Following.\n");
+	for (int i = 1; i <= b; i++)
+	{
+		for (int j = 0; j <= b - i; j++)
+			printf("  ");
+		for (int j = 0; j < i; j++)
+			printf(" *");
+		printf("\n");
+	}
+}
+
+void shape3(int b)
+{
+	puts("\nRequire Pattern Is Following.\n");
+	for (int i = 0; i < b; i++)
+	{
+		for (int j = 65; j <= (65 + i); j++)
+		{
+			printf(" %c ", j);
+		}
+		printf("\n");
+	}
+}
+
+void shape4(int b) //for squre
+{
+	puts("\nRequire Pattern Is Following.\n");
+	for (int i = 0; i < b; i++)
+		printf(" *");
+	printf("\n");
+
+	for (int i = 0; i < b - 2; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			if (j == 0)
+				printf(" *");
+			else if (j == b - 1)
+				printf(" *");
+			else
+				printf("  ");
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < b; i++)
+		printf(" *");
+}
+
+/*
+author- suraj kumar giri
+date of completion- 13th June 2021
+purpose- assignment work provided by Anand sir
+*/
---
+++ 02) Lab or Assignment works/04) Assignment 4.c
@@ -0,0 +1,318 @@
+//c language
+/*02) Lab or Assignment work> */
+/*04) Assignment 4.c*/
+//101
+//Assignment 4
+//done
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+void numbers(int p);
+void sum(int p, int q);
+void subtract(int p, int q);
+void multiply(int p, int q);
+void divide(int p, int q);
+void npr(int p, int q);
+void ncr(int p, int q);
+void hcf(int p, int q);
+void lcm(int p, int q);
+void power(int p, int q);
+void greater(int p, int q);
+void even_odd(int p, int q);
+void prime(int p, int q);
+int factorial(int n);
+int main()
+{
+	int n;
+	char ch[4];
+	do
+	{
+		puts("01. To Find sum of two given numbers");
+		puts("02. To Find subtraction two given numbers");
+		puts("03. To Find multiply of two given numbers");
+		puts("04. To Find division two given numbers");
+		puts("05. To Find npr");
+		puts("06. To Find ncr");
+		puts("07. To Find HCF of two given numbers");
+		puts("08. To Find LCM of two given numbers");
+		puts("09. To Find a to the power b");
+		puts("10. To Find which one is greater");
+		puts("11. To Find and Count odd and even numbers between two given numbers");
+		puts("12. To Find and count prime and composite numbers between two given number");
+
+		puts("\nWrite your choice:");
+		scanf("%d", &n);
+		numbers(n);
+		/*After complete execution of whole command the control 
+	    regain by main. so here if we do anything that will said to 
+	    be done in last part of whole program and get executed in last.
+	    */
+		//last part of whole program(i.e it will get executed in last)
+
+		puts("\n\nDo you want to restart the whole program. yes or no");
+		puts("Please write your choice:");
+		scanf("%s", ch);
+	} while (ch[0] == 'y' || ch[0] == 'Y');
+	printf("\033[1;33m");
+	puts("\n\t\tThanking You. Bye Bye........");
+	return 0;
+}
+void numbers(int p)
+{
+	puts("\n\t\t\t****WELCOME****\n");
+	int a, b;
+	printf("write the 1st number:\n");
+	scanf("%d", &a);
+	printf("write the 2nd number:\n");
+	scanf("%d", &b);
+	switch (p)
+	{
+	case 1:
+		sum(a, b);
+		break;
+
+	case 2:
+		subtract(a, b);
+		break;
+
+	case 3:
+		multiply(a, b);
+		break;
+
+	case 4:
+		divide(a, b);
+		break;
+
+	case 5:
+		npr(a, b);
+		break;
+
+	case 6:
+		ncr(a, b);
+		break;
+
+	case 7:
+		hcf(a, b);
+		break;
+
+	case 8:
+		lcm(a, b);
+		break;
+
+	case 9:
+		power(a, b);
+		break;
+
+	case 10:
+		greater(a, b);
+		break;
+
+	case 11:
+		even_odd(a, b);
+		break;
+
+	case 12:
+		prime(a, b);
+		break;
+
+	default:
+		printf("\nYou have selected wrong choice. Please reselect correct one\n");
+		main(); /*calling main to reinput the value. but main is called funtion
+		                 here so. In complete execution control again come to numbers() 
+		                 and in last the whole control regain by main() as initiated.                
+		                */
+	}
+}
+
+void sum(int p, int q)
+{
+	printf("\nThe sum of %d and %d is %d", p, q, p + q);
+}
+
+void subtract(int p, int q)
+{
+	printf("\nThe subtraction of %d from %d is %d", q, p, p - q);
+	printf("\nThe subtraction of %d from %d is %d", p, q, q - p);
+}
+
+void multiply(int p, int q)
+{
+	printf("\nThe Multiplication of %d and %d is %d", p, q, p * q);
+}
+
+void divide(int p, int q)
+{
+	printf("The quotient by diving %d by %d is %f\n", p, q, ((float)p / (float)q));
+	printf("The quotient by diving %d by %d is %f", q, p, ((float)q / (float)p));
+}
+void npr(int p, int q)
+{
+	int n, r;
+	n = factorial(p);
+	r = factorial(p - q);
+	printf("\nnpr of n=%d and r=%d is %f", p, q, (float)n / (float)r);
+}
+
+void ncr(int p, int q)
+{
+	int n, m, r;
+	n = factorial(p);
+	m = factorial(q);
+	r = factorial(p - q);
+	printf("\nncr of n=%d and r=%d is %f", p, q, ((float)n / ((float)m * (float)r)));
+}
+
+void hcf(int p, int q)
+{
+	int smaller;
+	if (p < q)
+		smaller = p;
+	else
+		smaller = q;
+	while (smaller > 1)
+	{
+		if (p % smaller == 0 && q % smaller == 0)
+			break;
+		smaller--;
+	}
+	printf("\n%d is the HCF of %d and %d", smaller, p, q);
+}
+
+void lcm(int p, int q)
+{
+	int greater1;
+	if (p > q)
+		greater1 = p;
+	else
+		greater1 = q;
+	for (int i = 1; i > 0; i++)
+	{
+		if (greater1 % p == 0 && greater1 % q == 0)
+			break;
+		greater1 = greater1 * i;
+	}
+	printf("\nLCM of %d and %d is %d", p, q, greater1);
+}
+void power(int p, int q)
+{
+	printf("\npower of %d to %d is %f", q, p, pow((float)p, (float)q));
+	printf("\npower of %d to %d is %f", p, q, pow((float)q, (float)p));
+}
+void greater(int p, int q)
+{
+	int greater1;
+	if (p > q)
+		greater1 = p;
+	else
+		greater1 = q;
+	printf("\n%d is greater in %d and %d", greater1, p, q);
+}
+void even_odd(int p, int q)
+{
+	int greater1;
+	int smaller;
+	if (p > q)
+	{
+		greater1 = p;
+		smaller = q;
+	}
+	else
+	{
+		greater1 = q;
+		smaller = p;
+	}
+	int i, j, even[greater1 - smaller], odd[greater1 - smaller];
+	int count1 = 0, count2 = 0;
+	i = 0;
+	j = 0;
+	smaller = smaller + 1; //because we have to find no.s b/w them, not them
+	while (smaller < greater1)
+	{
+		if (smaller % 2 == 0)
+		{
+			even[i] = smaller;
+			count1 = count1 + 1;
+			i++;
+		}
+		else
+		{
+			odd[j] = smaller;
+			count2 = count2 + 1;
+			j++;
+		}
+		smaller++;
+	}
+	printf("\nEven Numbers Between %d and %d are following:-\n", p, q);
+	for (i = 0; i < count1; i++)
+		printf("%02d\t", even[i]);
+	printf("\n\nTotal number of even numbers are %d", count1);
+
+	printf("\n\nOdd Numbers Between %d and %d are following:-\n", p, q);
+	for (i = 0; i < count2; i++)
+		printf("%02d\t", odd[i]);
+	printf("\n\nTotal number of even numbers are %d", count2);
+}
+void prime(int p, int q)
+{
+	int greater1;
+	int smaller;
+	if (p > q)
+	{
+		greater1 = p;
+		smaller = q;
+	}
+	else
+	{
+		greater1 = q;
+		smaller = p;
+	}
+	int i = 0, j = 0, prime[greater1 - smaller], composite[greater1 - smaller];
+	int count1 = 0, count2 = 0;
+	smaller = smaller + 1;
+	int k, check;
+	while (smaller < greater1)
+	{
+		check = 0;
+		for (k = 2; k < smaller; k++)
+		{
+			if (smaller % k == 0)
+				check = check + 1;
+		}
+		if (check != 0)
+		{
+			count1 = count1 + 1; //for composite
+			composite[i] = smaller;
+			i++;
+		}
+		else
+		{
+			count2 = count2 + 1;
+			prime[j] = smaller;
+			j++;
+		}
+		smaller++;
+	}
+	printf("\nComposite Numbers Between %d and %d are following:-\n", p, q);
+	for (i = 0; i < count1; i++)
+		printf("%02d\t", composite[i]);
+	printf("\n\nTotal number of composite numbers are %d", count1);
+
+	printf("\n\nPrime Numbers Between %d and %d are following:-\n", p, q);
+	for (i = 0; i < count2; i++)
+		printf("%02d\t", prime[i]);
+	printf("\n\nTotal number of prime numbers are %d", count2);
+}
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+		return 1;
+	else
+		return (n * (factorial(n - 1))); //recursion
+}
+
+/*
+author- suraj kumar giri
+date of completion- 13th June 2021
+purpose- assignment work provided by Anand sir
+*/
