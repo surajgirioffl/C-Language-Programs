3656d93c63fdcdb4af966f068b0a5a00709600a0
surajgirioffl <surajkumargiri2002@gmail.com>
2022-01-09 12:58:03 +0530

    added c program to create any numbers of directory using loop

---
+++ #01) All In One C Programs (mixture of any of concepts of C)/16)create_1000_directories.c
@@ -0,0 +1,17 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    char command[200] = "mkdir this_is_me";
+    for (int i = 1; i <= 1000; i++)
+    {
+        sprintf(command, "mkdir \"D:\\C Language\\folder\\new_directory_%d\"", i);
+        if (system(command)) // system function return 0 on failure
+            printf("Failed to create folder\n");
+        else
+            printf("Directory created successfully %d\n", i);
+    }
+    return 0;
+}
+// we use "mkdir new_directory_name" to create directory in current working directory.
+// we use "mkdir path/new_directory_name" to create directory in any other directory of local filesystem
