ad1cdc8dea64c1f3afebe0cdab9dcae0e6826fa0
surajgirioffl <surajkumargiri2002@gmail.com>
2022-01-09 12:57:10 +0530

    added c program to create any numbers of file using loop

---
+++ #01) All In One C Programs (mixture of any of concepts of C)/15)create_1000_files.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+    char filename_with_path[200];
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        sprintf(filename_with_path, "D:\\C Language\\All C Programs\\1) Cxxdroid Application\\Repositories\\C Language Special\\C_Language_Programs\\#01) All In One C Programs (mixture of any of concepts of C)\\folder\\filename_%d.txt", i);
+        FILE *ptr = fopen(filename_with_path, "w");
+        printf("file create successfully %d\n", i);
+        fclose(ptr);
+    }
+    return 0;
+}
